68e19798eebb2dd437053daf1814f23ff597f576
TaikiHoshika <21060016ht@edu.fca.ac.jp>
2024-11-08 15:29:57 +0900

    [test] test

--- .github/workflows/setPackageInfomation.ts
+++ .github/workflows/setPackageInfomation.ts
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const data = JSON.parse(fs.readFileSync(process.argv[2], "utf8"));
-data["repository"]["url"] = process.argv[3];
+data["name"] = `@${process.argv[3]}/${process.argv[4]}`;
+data["repository"]["url"] = process.argv[5];
 
 fs.writeFileSync(process.argv[2], JSON.stringify(data), "utf8");
